ff193fdc6179e4360cbfa1a35f5f113942111689
minhwvu <minh2510@gmail.com>
2023-07-11 14:51:57 -0400

    give some structure

--- molecool/__init__.py
+++ molecool/__init__.py
@@ -1,7 +1,14 @@
-"""A Python package to visualize molecules given their Cartesian coordinates"""
+"""A Python package to visualize molecules given their Cartesian coordinates.
+This was developed for the Python Best Practices Workshop."""
 
 # Add imports here
 from .functions import *
+from molecool.measure import calculate_angle
+from molecool.measure import calculate_distance
+from molecool.atom_data import atomic_weights, atom_colors
+from molecool.visualization import draw_molecule
+from molecool.molecules import build_bond_list, bond_histogram
 
+from molecool.io import open_pdb
 
 from ._version import __version__
